a6f4bfdf26befeb707806ac1dc65fdf20fa25451
limgyuil <50009192+gyuilLim@users.noreply.github.com>
2024-03-29 20:40:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import feedparser
 blog_url = "https://mvcv.tistory.com/rss"
 rss_feed = feedparser.parse(blog_url)
 
-MAX_NUM = 5
+MAX_NUM = 3
 
 latest_posts = ""
 
@@ -15,7 +15,26 @@ for idx, entrie in enumerate(rss_feed['entries']):
   latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
 
 preREADME = """
-## 기존의 README.md 내용
+# 👋 안녕하세요. 임규일입니다.
+- (현)SSU Vision Lab
+- (현)숭실대학교 AI융합학부
+
+## 📪 Contact
+
+- `Email` | swea76019059@gmail.com
+- `TechBlog` | <a href="https://stg0123.github.io/" target="_blank">https://mvcv.tistory.com/</a>
+
+## 🏁 AI challenge
+- 2023 SW중심대학 공동 AI 경진대회 SW중심대학협의회장상
+- 2023 2023 Samsung AI Challenge : Camera-Invariant Domain Adaptation 10등
+
+## ⌚ 연혁<br/>
+|활동|기간|비고|
+|---|---|---|
+|숭실대학교 AI융합학부|2019.03~ | 숭실대학교 AI융합학부 소속|
+|공군 군사경찰 |2021.01~2022.10|공군 병장 만기제대|
+|숭실대학교 Vision Lab | 2023.06~ | Computer Vision |
+
 """
 
 resultREADME = f"{preREADME}{latest_posts}"
